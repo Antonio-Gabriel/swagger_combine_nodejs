0196fe74180383e2728ded32ba4e00f559ff73ef
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-02-25 15:37:08 +0100

    segregate routes

--- src/index.js
+++ src/index.js
@@ -1,45 +1,9 @@
 const express = require("express");
-const { v4: uuidv4 } = require("uuid");
+const { router } = require("./routes");
 
 const app = new express();
 
 app.use(express.json());
+app.use("/v1", router);
 
-const storage = [];
-
-app.post("employees/register", function (req, res) {
-  const id = uuidv4();
-  const { name, username, email } = req.body;
-
-  const employeeAlreadyExists = storage.find(
-    (employee) => employee.name === name
-  );
-
-  if (!employeeAlreadyExists) {
-    throw new Error("Employee already exists");
-  }
-
-  const data = {
-    id,
-    name,
-    username,
-    email,
-  };
-
-  storage.push(data);
-
-  return res.status(201).json(data);
-});
-
-app.get("/employees", function (req, res) {
-  return res.json(storage);
-});
-
-app.get("/employees/:id", function (req, res) {
-  const { id } = req.query;
-  const employee = storage.find((employee) => employee.id === id);
-
-  return res.json(employee);
-});
-
-app.listen(3333);
+app.listen(3333, () => console.log("Server is running on port 3333"));
---
+++ src/routes.js
@@ -0,0 +1,50 @@
+const { Router } = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+const router = Router();
+
+const storage = [];
+
+router.post("employees/register", function (req, res) {
+  const { name, username, email } = req.body;
+
+  const employeeAlreadyExists = storage.find(
+    (employee) => employee.name === name
+  );
+
+  if (employeeAlreadyExists) {
+    return res.status(400).json({
+      message: "Employee Already Exists!",
+    });
+  }
+
+  const data = {
+    id: uuidv4(),
+    name,
+    username,
+    email,
+  };
+
+  storage.push(data);
+
+  return res.status(201).json(data);
+});
+
+router.get("/employees", function (req, res) {
+  return res.json(storage);
+});
+
+router.get("/employees/:id", function (req, res) {
+  const { id } = req.query;
+  const employee = storage.find((employee) => employee.id === id);
+
+  return res.json(employee);
+});
+
+router.get("/terms", function (req, res) {
+  return res.json({
+    message: "Terms of Service",
+  });
+});
+
+module.exports = { router };
