251124efb7ac0062a1c992a63b760b5c2a9b96cf
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-02-25 18:43:21 +0100

    apply deepmerge

--- src/index.js
+++ src/index.js
@@ -8,13 +8,17 @@ const swaggerDocsDev = require("./swagger.json");
 const swaggerUi = require("swagger-ui-express");
 const _ = require("lodash");
 
+const { mergeDeep } = require("./utils/DeepMerge");
+
 const app = new express();
 
 app.use(express.json());
 app.use("/v1", router);
 
-//const swaggerMerge = { ...swaggerDocsDev, ...swaggerDocsProd };
-const swaggerMerge = _.merge(swaggerDocsDev, swaggerDocsProd);
+// const swaggerMerge = { ...swaggerDocsDev, ...swaggerDocsProd };
+// const swaggerMerge = _.merge(swaggerDocsDev, swaggerDocsProd);
+const swaggerMerge =  mergeDeep(swaggerDocsDev, swaggerDocsProd);
+console.log(swaggerMerge);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerMerge));
 
