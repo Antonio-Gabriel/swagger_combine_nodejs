94f6e35ee9ad7e6fa663876ad36c6bf7aa3f5e75
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-02-25 16:01:53 +0100

    add terms route

--- src/index.js
+++ src/index.js
@@ -9,4 +9,10 @@ app.use(express.json());
 app.use("/v1", router);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
+app.get("/terms", function (req, res) {
+  return res.json({
+    message: "Terms of Service",
+  });
+});
+
 app.listen(3333, () => console.log("Server is running on port 3333"));
