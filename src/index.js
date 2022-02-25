338f3567e51027607cc619272287823966ab1928
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-02-25 15:56:32 +0100

    add swagger and configure setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { router } = require("./routes");
+const swaggerDocs = require("./swagger.json");
+const swaggerUi = require("swagger-ui-express");
 
 const app = new express();
 
 app.use(express.json());
 app.use("/v1", router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
